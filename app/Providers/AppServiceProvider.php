089b4826da438b1a56db079dd08899157825a0fc
kazukogo <183983447+kazukogo@users.noreply.github.com>
2025-03-09 18:48:39 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    if(\App::enviroment(['prduction'])){
+        \URL::forceScheme('https');   
     }
 }
